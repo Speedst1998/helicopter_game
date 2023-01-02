6177ee799f0b484f936eabd7bca79e7b6a19ba1c
Tahar Mustapha <mtahar1998@gmail.com>
2023-01-02 14:21:47 -0500

    removed build folder and .vscode folder from repo

--- build/third_party/glad/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Dev/helicopter_game/third_party/glad
-# Build directory: C:/Dev/helicopter_game/build/third_party/glad
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
